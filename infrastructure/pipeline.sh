67edbbef8926ea20e27f9ba98cd5e4f1222bc4b8
sambhavgupta13 <sambhav.gupta@comprotechnologies.com>
2023-12-06 12:42:11 +0530

    added template for deploying resources

---
+++ infrastructure/pipeline.sh
@@ -0,0 +1,46 @@
+yell() { echo "$0: $*" >&2; }
+die() { yell "$*"; exit 111; }
+try() { eval "$@" || die "cannot $*"; }
+
+#Run command based on action provided
+case $1 in
+    'validate')
+        svalidate="sam.cmd validate --template-file infrastructure/pipeline.yaml"
+        try $svalidate
+
+        yell "SAM:Building application"
+        sbuild="sam.cmd build --template-file infrastructure/pipeline.yaml"
+        try $sbuild
+        ;;
+
+    'create')
+        yell "Creating CloudFormation Stack"
+
+        aws cloudformation create-stack \
+          --capabilities CAPABILITY_IAM \
+          --stack-name pipeline-cf-stack \
+          --region us-east-1 \
+          --template-body file://infrastructure/pipeline.yaml
+        ;;
+
+    'update')
+        yell "Updating CloudFormation Stack"
+
+        aws cloudformation update-stack \
+          --capabilities CAPABILITY_IAM \
+          --stack-name pipeline-cf-stack \
+          --region us-east-1 \
+          --template-body file://infrastructure/pipeline.yaml
+        ;;
+    
+    'delete')
+        yell "Deleting CloudFormation Stack"
+
+        aws cloudformation delete-stack \
+          --stack-name pipeline-cf-stack
+        ;;
+        
+    *)
+        die "Unsupported action: $1"
+        ;;
+esac
