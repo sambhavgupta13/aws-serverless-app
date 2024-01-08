a5741b05a177927fb2ebf138b852df3e0c3c6796
sambhavgupta13 <sambhav.gupta@comprotechnologies.com>
2024-01-08 17:13:27 +0530

    test

--- infrastructure/sam_dls.sh
+++ infrastructure/sam_dls.sh
@@ -20,20 +20,20 @@ case $1 in
     'package')
         # building and pushing docker image to ECR repository
         yell "SAM: Logging into Amazon ECR  Repository"
-        slogin="aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 189731456380.dkr.ecr.us-east-1.amazonaws.com"
-        try $slogin
+        # slogin="aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 189731456380.dkr.ecr.us-east-1.amazonaws.com"
+        # try $slogin
         
-        yell "SAM: Building the Docker image of ${config_ecr_repository_name}"
-        sbuild="docker build -t sambhav-test-ecr:latest -f infrastructure/Dockerfile ."
-        try $sbuild
+        # yell "SAM: Building the Docker image of ${config_ecr_repository_name}"
+        # sbuild="docker build -t sambhav-test-ecr:latest -f infrastructure/Dockerfile ."
+        # try $sbuild
 
-        yell "SAM: Tagging Docker image with ${config_ecr_repository_name} ECR Repository"
-        stag="docker tag sambhav-test-ecr:latest 189731456380.dkr.ecr.us-east-1.amazonaws.com/sambhav-test-ecr:latest"
-        try $stag
+        # yell "SAM: Tagging Docker image with ${config_ecr_repository_name} ECR Repository"
+        # stag="docker tag sambhav-test-ecr:latest 189731456380.dkr.ecr.us-east-1.amazonaws.com/sambhav-test-ecr:latest"
+        # try $stag
 
-        yell "SAM: Pushing the Docker image into ${config_ecr_repository_name} ECR Repository"
-        spush="docker push 189731456380.dkr.ecr.us-east-1.amazonaws.com/sambhav-test-ecr:latest"
-        try $spush
+        # yell "SAM: Pushing the Docker image into ${config_ecr_repository_name} ECR Repository"
+        # spush="docker push 189731456380.dkr.ecr.us-east-1.amazonaws.com/sambhav-test-ecr:latest"
+        # try $spush
         ;;
 
     'deploy')
