8d3e221d5a6d3d48b9401bb854fcf896ab238e66
sambhavgupta13 <sambhav.gupta@comprotechnologies.com>
2024-01-11 13:58:01 +0530

    test again

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,7 +9,7 @@
    <h4>testing commit 3</h4>
    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat, commodi aliquam dolor incidunt tenetur natus laboriosam, tempora excepturi consequatur temporibus deleniti sit quis a beatae repudiandae voluptatem vero? Repellat, a.</p>
   <h2>Test Commit</h2>
-  <h2>New Feature added</h2>
+  <h2>New Feature added  </h2>
   <h1>Test Commit       </h1>
   </div>
 </template>
