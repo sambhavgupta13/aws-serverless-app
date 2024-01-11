ac8027e2231aa881a9714e8c87dba77a3c5c9915
sambhavgupta13 <sambhav.gupta@comprotechnologies.com>
2024-01-11 13:30:59 +0530

    testing

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,8 +4,8 @@ import App from './App.vue'
 Vue.config.productionTip = false
 console.log("main.js")
 console.log("test")
-console.log("commit")
-console.log("web hook  ")
+console.log("commit") 
+console.log("web hook   ")
 new Vue({
   render: h => h(App),
 }).$mount('#app')
