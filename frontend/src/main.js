a1e45bcd43d0ee1f9002850c2e827d7dc9a5040a
sambhavgupta13 <sambhav.gupta@comprotechnologies.com>
2024-01-11 13:22:28 +0530

    testing

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,7 +5,7 @@ Vue.config.productionTip = false
 console.log("main.js")
 console.log("test")
 console.log("commit")
-console.log("web hook")
+console.log("web hook  ")
 new Vue({
   render: h => h(App),
 }).$mount('#app')
